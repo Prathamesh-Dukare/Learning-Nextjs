8851bf491b12b289fdf810240c1c7ef9126c2f1d
Prathamesh Dukare <Prathameshdukaremail@gmail.com>
2023-01-21 19:25:47 +0530

    started learning nextjs

---
+++ Nextjs basics/pages/index.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+export default function HomePage() {
+  const names = ["Ada Lovelace", "Grace Hopper", "Margaret Hamilton"];
+  const [likes, setLikes] = useState(0);
+
+  function handleClick() {
+    setLikes(likes + 1);
+  }
+
+  return (
+    <div>
+      <ul>
+        {names.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+
+      <button onClick={handleClick}>Like ({likes})</button>
+    </div>
+  );
+}
